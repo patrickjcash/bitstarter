f19b75893c6eed096ae9e3fc1605f6653615c87c
Patrick Cash <patrick.j.cash@gmail.com>
2013-07-11 17:17:40 +0000

    retry hello world

--- web.js
+++ web.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
-
+/*
 fs.readFileSync('/index.html', function (err, data) {
   if (err) throw err;
   var buffer = new Buffer(data);
 });
 
 var str = buffer.toString('utf-8');
-
+*/
 app.get('/', function(request, response) {
-  response.send(str);
+  response.send('Hello world 3!');
 });
 
 var port = process.env.PORT || 5000;
