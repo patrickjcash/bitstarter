5bbf8a63033319faa0a16c65f0e06f6ebdd367b5
Patrick Cash <patrick.j.cash@gmail.com>
2013-07-11 18:10:13 +0000

    try again with web.js reading from file

--- web.js
+++ web.js
@@ -1,19 +1,15 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
-/*
-fs.readFileSync('/index.html', function (err, data) {
-  if (err) throw err;
-  var buffer = new Buffer(data);
-});
 
-var str = buffer.toString('utf-8');
-*/
 app.get('/', function(request, response) {
-  response.send('Hello world 3!');
+  var content = new Buffer(fs.readFileSync('index.html'),'utf-8');
+//  console.log(content.toString());
+  response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
