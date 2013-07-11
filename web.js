d506008edfc1a08227a8f4b517b56bf5071ce34e
Patrick Cash <patrick.j.cash@gmail.com>
2013-07-11 17:15:07 +0000

    Fixing web.js to include fs require

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 fs.readFileSync('/index.html', function (err, data) {
   if (err) throw err;
