fb3e0cea59ca29195b75a6d5cb51a983d127e02e
Patrick Cash <patrick.j.cash@gmail.com>
2013-07-11 17:09:34 +0000

    Modified web.js to read from index.html using buffers

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+fs.readFileSync('/index.html', function (err, data) {
+  if (err) throw err;
+  var buffer = new Buffer(data);
+});
+
+var str = buffer.toString('utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
